85ce6b7489d1f7c819d5c36db444cb741d1e3fdc
mitomtrung98 <101630962+mitomtrung98@users.noreply.github.com>
2023-02-01 10:54:38 +0700

    a
    a

---
+++ JavaExam/src/javaexam/Phone.java
@@ -0,0 +1,12 @@
+
+package javaexam;
+
+
+    public abstract class Phone {
+    abstract void insertPhone(String Name, String Phone);
+    abstract void removePhone(String Name);
+    abstract void deletePhone(String Name, String newPhone);
+    abstract void searchPhone(String Name);
+    abstract void sort();
+
+}
